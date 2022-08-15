c7eba81f9f024420e03d4fed495bac5c22a4b1c9
yushulx <lingxiao1002@gmail.com>
2022-08-15 10:16:11 +0800

    Decode barcode Qr code in a worker thread with a task queue.

--- BarcodeReader.cxx
+++ BarcodeReader.cxx
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <functional>
 
 using namespace std;
 using namespace dynamsoft::dbr;
@@ -22,6 +26,94 @@ int gettime()
 	return (int)(GetTickCount());
 }
 #endif
+
+class WorkerThread
+{
+public:
+	std::mutex m;
+	std::condition_variable cv;
+	std::queue<std::function<void()>> tasks = {};
+	volatile bool running;
+	std::thread t;
+	void *reader;
+};
+
+void run(WorkerThread *worker)
+{
+	while (worker->running)
+	{
+		std::function<void()> task;
+		std::unique_lock<std::mutex> lk(worker->m);
+		worker->cv.wait(lk, [&]
+						{ return !worker->tasks.empty() || !worker->running; });
+		if (!worker->running)
+		{
+			break;
+		}
+		task = std::move(worker->tasks.front());
+		worker->tasks.pop();
+		lk.unlock();
+
+		task();
+	}
+}
+
+void decode(WorkerThread *worker, const unsigned char *buffer, int size, int formats, int threadcount, char *config)
+{
+	std::thread::id thread_id = std::this_thread::get_id();
+
+	// Load the configuration from a template file
+	if (config)
+	{
+		char szErrorMsg[256];
+		int ret = DBR_InitRuntimeSettingsWithString(worker->reader, config, CM_OVERWRITE, szErrorMsg, 256);
+		if (ret)
+			printf("Template status: %s\n\n", szErrorMsg);
+	}
+
+	// Update the parameters
+	char sError[512];
+	PublicRuntimeSettings *runtimeSettings = new PublicRuntimeSettings();
+	DBR_GetRuntimeSettings(worker->reader, runtimeSettings);
+	runtimeSettings->maxAlgorithmThreadCount = threadcount;
+	runtimeSettings->barcodeFormatIds = formats;
+	DBR_UpdateRuntimeSettings(worker->reader, runtimeSettings, sError, 512);
+	delete runtimeSettings;
+
+	// Read barcodes from file stream
+	int starttime = gettime();
+	int iRet = DBR_DecodeFileInMemory(worker->reader, buffer, (int)size, "");
+	int endtime = gettime();
+	int timecost = endtime - starttime;
+
+	// Output barcode result
+	if (iRet != DBR_OK && iRet != DBRERR_MAXICODE_LICENSE_INVALID && iRet != DBRERR_AZTEC_LICENSE_INVALID && iRet != DBRERR_LICENSE_EXPIRED && iRet != DBRERR_QR_LICENSE_INVALID && iRet != DBRERR_GS1_COMPOSITE_LICENSE_INVALID &&
+		iRet != DBRERR_1D_LICENSE_INVALID && iRet != DBRERR_PDF417_LICENSE_INVALID && iRet != DBRERR_DATAMATRIX_LICENSE_INVALID && iRet != DBRERR_GS1_DATABAR_LICENSE_INVALID && iRet != DBRERR_PATCHCODE_LICENSE_INVALID)
+	{
+		printf("Failed to read barcode: %s\n", DBR_GetErrorString(iRet));
+	}
+
+	TextResultArray *paryResult = NULL;
+	DBR_GetAllTextResults(worker->reader, &paryResult);
+
+	if (paryResult->resultsCount == 0)
+	{
+		printf("No barcode found.\n");
+		DBR_FreeTextResults(&paryResult);
+		return;
+	}
+
+	printf("Thread id: %d. Total barcode(s) found: %d. Time cost: %d ms\n\n", thread_id, paryResult->resultsCount, timecost);
+
+	for (int index = 0; index < paryResult->resultsCount; index++)
+	{
+		printf("Barcode %d:\n", index + 1);
+		printf("    Type: %s\n", paryResult->results[index]->barcodeFormatString);
+		printf("    Text: %s\n", paryResult->results[index]->barcodeText);
+	}
+
+	DBR_FreeTextResults(&paryResult);
+}
 
 char *read_file_text(const char *filename)
 {
@@ -94,71 +186,6 @@ unsigned char *read_file_binary(const char *filename, int *out_size)
 	return buffer;
 }
 
-int barcode_decoding(const unsigned char *buffer, int size, int formats, int threadcount, char *config)
-{
-	std::thread::id thread_id = std::this_thread::get_id();
-	// Initialize Dynamsoft Barcode Reader
-	void *reader = DBR_CreateInstance();
-	const char *version = DBR_GetVersion();
-	printf("DBR version: %s\n", version);
-
-	// Load the configuration from a template file
-	if (config)
-	{
-		char szErrorMsg[256];
-		int ret = DBR_InitRuntimeSettingsWithString(reader, config, CM_OVERWRITE, szErrorMsg, 256);
-		if (ret)
-			printf("Template status: %s\n\n", szErrorMsg);
-	}
-
-	// Update the parameters
-	char sError[512];
-	PublicRuntimeSettings *runtimeSettings = new PublicRuntimeSettings();
-	DBR_GetRuntimeSettings(reader, runtimeSettings);
-	runtimeSettings->maxAlgorithmThreadCount = threadcount;
-	runtimeSettings->barcodeFormatIds = formats;
-	DBR_UpdateRuntimeSettings(reader, runtimeSettings, sError, 512);
-	delete runtimeSettings;
-
-	// Read barcodes from file stream
-	int starttime = gettime();
-	int iRet = DBR_DecodeFileInMemory(reader, buffer, (int)size, "");
-	int endtime = gettime();
-	int timecost = endtime - starttime;
-
-	// Output barcode result
-	if (iRet != DBR_OK && iRet != DBRERR_MAXICODE_LICENSE_INVALID && iRet != DBRERR_AZTEC_LICENSE_INVALID && iRet != DBRERR_LICENSE_EXPIRED && iRet != DBRERR_QR_LICENSE_INVALID && iRet != DBRERR_GS1_COMPOSITE_LICENSE_INVALID &&
-		iRet != DBRERR_1D_LICENSE_INVALID && iRet != DBRERR_PDF417_LICENSE_INVALID && iRet != DBRERR_DATAMATRIX_LICENSE_INVALID && iRet != DBRERR_GS1_DATABAR_LICENSE_INVALID && iRet != DBRERR_PATCHCODE_LICENSE_INVALID)
-	{
-		printf("Failed to read barcode: %s\n", DBR_GetErrorString(iRet));
-		// return 0;
-	}
-
-	TextResultArray *paryResult = NULL;
-	DBR_GetAllTextResults(reader, &paryResult);
-
-	if (paryResult->resultsCount == 0)
-	{
-		printf("No barcode found.\n");
-		DBR_FreeTextResults(&paryResult);
-		return -1;
-	}
-
-	printf("Thread id: %d. Total barcode(s) found: %d. Time cost: %d ms\n\n", thread_id, paryResult->resultsCount, timecost);
-
-	for (int index = 0; index < paryResult->resultsCount; index++)
-	{
-		printf("Barcode %d:\n", index + 1);
-		printf("    Type: %s\n", paryResult->results[index]->barcodeFormatString);
-		printf("    Text: %s\n", paryResult->results[index]->barcodeText);
-	}
-
-	DBR_FreeTextResults(&paryResult);
-
-	DBR_DestroyInstance(reader);
-	return timecost;
-}
-
 void ToHexString(unsigned char *pSrc, int iLen, char *pDest)
 {
 	const char HEXCHARS[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
@@ -173,35 +200,11 @@ void ToHexString(unsigned char *pSrc, int iLen, char *pDest)
 	}
 }
 
-void multi_thread_performance(int processor_count, unsigned char *buffer, int size, int formats, char *config)
-{
-	int minimum_count = 1, minimum_timecost = 0;
-	for (int i = 0; i < processor_count; i++)
-	{
-		printf("Thread count: %d. ", i + 1);
-		int timecost = barcode_decoding(buffer, size, formats, i, config);
-		if (i == 0)
-		{
-			minimum_count = 1;
-			if (timecost > 0)
-			{
-				minimum_timecost = timecost;
-			}
-		}
-		else
-		{
-			if (timecost < minimum_timecost)
-			{
-				minimum_count = i + 1;
-				minimum_timecost = timecost;
-			}
-		}
-	}
-	printf("Multi-thread best performance: thread_count = %d, timecost = %d \n\n", minimum_count, minimum_timecost);
-}
-
 int main(int argc, const char *argv[])
 {
+	const char *version = DBR_GetVersion();
+	printf("DBR version: %s\n", version);
+
 	const auto processor_count = std::thread::hardware_concurrency();
 	printf("CPU threads: %d\n\n", processor_count);
 
@@ -236,42 +239,36 @@ int main(int argc, const char *argv[])
 	if (!buffer)
 		return 0;
 
-	// Call decoding methods on the main thread
-	printf("---------------- Single thread decoding performance ----------------\n\n");
-	printf("---------------- Decoding barcodes on main thread ----------------\n\n");
-	// barcode_decoding(buffer, size, BF_ONED, 1, config);
-	// barcode_decoding(buffer, size, BF_CODE_39, 1, config);
+	WorkerThread *worker = new WorkerThread();
+	worker->reader = DBR_CreateInstance();
+	worker->running = true;
+	worker->t = std::thread(&run, worker);
 
-	// barcode_decoding(buffer, size, BF_QR_CODE, 1, config);
-	// barcode_decoding(buffer, size, BF_PDF417, 1, config);
-	// barcode_decoding(buffer, size, BF_DATAMATRIX, 1, config);
-	// barcode_decoding(buffer, size, BF_DATAMATRIX | BF_QR_CODE | BF_PDF417, 1, config);
-	// barcode_decoding(buffer, size, BF_ALL, 1, config);
+	// put tasks to the queue
+	for (int i = 0; i < 5; i++)
+	{
+		std::unique_lock<std::mutex> lk(worker->m);
+		if (worker->tasks.size() > 1)
+		{
+			std::queue<std::function<void()>> empty = {};
+			std::swap(worker->tasks, empty);
+		}
 
-	// Call decoding methods on worker threads
-	printf("---------------- Decoding barcodes on worker threads ----------------\n\n");
-	int starttime = gettime();
-	// thread t1(barcode_decoding, buffer, size, BF_ONED);
-	// thread t2(barcode_decoding, buffer, size, BF_QR_CODE, 1, config);
-	// thread t3(barcode_decoding, buffer, size, BF_PDF417, 1, config);
-	// thread t4(barcode_decoding, buffer, size, BF_DATAMATRIX, 1, config);
-	// t1.join();
-	// t2.join();
-	// t3.join();
-	// t4.join();
-	int endtime = gettime();
-	printf("Thread time cost: %d ms\n\n", (endtime - starttime));
+		std::function<void()> task_function = std::bind(decode, worker, buffer, size, BF_ALL, 1, config);
+		worker->tasks.push(task_function);
+		worker->cv.notify_one();
+		lk.unlock();
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+	}
 
-	printf("---------------- Multi thread decoding performance ----------------\n\n");
-	// 1D
-	// printf("-------------------------------- 1D --------------------------------\n\n");
-	// multi_thread_performance((int)processor_count, buffer, size, BF_ONED, config);
-	// QR
-	// printf("-------------------------------- QR --------------------------------\n\n");
-	// multi_thread_performance((int)processor_count, buffer, size, BF_QR_CODE, config);
-	// All
-	printf("-------------------------------- All --------------------------------\n\n");
-	multi_thread_performance((int)processor_count, buffer, size, BF_ALL, config);
+	// terminate the thread
+	worker->running = false;
+	worker->cv.notify_one();
+	worker->t.join();
+
+	DBR_DestroyInstance(worker->reader);
+	delete worker;
+	worker = NULL;
 
 	free(config);
 	free(buffer);
