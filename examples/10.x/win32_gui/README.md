61d3347f29b39bc32bc69f7773789220e9a3871a
yushulx <lingxiao1002@gmail.com>
2024-06-03 16:38:44 +0800

    Added a Win32 GUI example

---
+++ examples/10.x/win32_gui/README.md
@@ -0,0 +1,16 @@
+# Windows Desktop C++ Barcode Reader 
+The project demonstrates how to build a Windows desktop barcode reader app using Win32 C++ API and Dynamsoft C++ Barcode SDK.
+
+## Getting Started
+1. Import the project to Visual Studio 2022.
+2. Request a [free trial license](https://www.dynamsoft.com/customer/license/trialLicense?product=dbr) and update the license key in the `BarcodeReader.cpp` file.
+
+    ```cpp
+    const char* pLicense = "LICENSE-KEY";
+    ```
+3. Build and run the project.
+
+    ![windows-win32-cpp-barcode-reader](https://github.com/yushulx/windows-win32-cpp-barcode-reader/assets/2202306/688fb2ca-5f10-4643-bc00-48b648dce47f)
+
+## Blog
+[Building Windows Desktop Barcode Reader with Win32 API and Dynamsoft C++ Barcode SDK](https://www.dynamsoft.com/codepool/windows-win32-cpp-barcode-reader.html)
---
+++ examples/10.x/win32_gui/Resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by BarcodeReader.rc
+
+#define IDS_APP_TITLE 103
+
+#define IDR_MAINFRAME 128
+#define IDD_BARCODEREADER_DIALOG 102
+#define IDD_ABOUTBOX 103
+#define IDM_ABOUT 104
+#define IDM_EXIT 105
+#define IDI_BARCODEREADER 107
+#define IDI_SMALL 108
+#define IDC_BARCODEREADER 109
+#define IDC_MYICON 2
+#define IDC_LOADIMAGE_BUTTON 1001
+#define IDC_TEXTBOX 1002
+#define IDC_IMAGE_PANEL 1003
+#define IDC_RESULT_PANEL 1004
+#define IDC_TEMPLATE_PANEL 1005
+#define IDC_LABEL 1006
+#ifndef IDC_STATIC
+#define IDC_STATIC -1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC 130
+#define _APS_NEXT_RESOURCE_VALUE 129
+#define _APS_NEXT_COMMAND_VALUE 32771
+#define _APS_NEXT_CONTROL_VALUE 1000
+#define _APS_NEXT_SYMED_VALUE 110
+#endif
+#endif
